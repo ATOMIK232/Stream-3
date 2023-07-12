647bd44b98b02d24884576307e2d5e8e7f902d1e
ATOM <maksimuslakt@gmail.com>
2023-07-12 15:26:36 +0300

    Stream 3

---
+++ Src/StreamMain.java
@@ -0,0 +1,6 @@
+public class StreamMain extends Main {
+    int[] nums = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+        for(int i = 0; i < nums.lenght(); i++) {
+        if(i > 0 && i % 2 == 0);
+        System.out.println(nums[i]); }
+}
